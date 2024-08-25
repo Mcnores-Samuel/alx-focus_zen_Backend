b55fe7b120f7868213faa73b2a2190c6607b7f66
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-08-25 23:02:30 +0200

    adding a requirents file for deployments of the api

--- alxfocuszen_engine/admin.py
+++ alxfocuszen_engine/admin.py
@@ -2,7 +2,7 @@ from alxfocuszen_engine.control_center.user_admin import UserProfileAdmin
 from alxfocuszen_engine.control_center.tasks_admin import TasksAdmin
 from alxfocuszen_engine.control_center.pomodoroSession_admin import PomodoroSessionAdmin
 from alxfocuszen_engine.models.user import UserProfile
-from alxfocuszen_engine.models.tasks import Tasks
+from alxfocuszen_engine.models.tasks import Task
 from alxfocuszen_engine.models.pomodoroSessions import PomodoroSession
 from django.contrib import admin
 
@@ -13,5 +13,5 @@ admin.site.index_title = 'Welcome to ALX Focus Zen Admin'
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Tasks, TasksAdmin)
+admin.site.register(Task, TasksAdmin)
 admin.site.register(PomodoroSession, PomodoroSessionAdmin)
